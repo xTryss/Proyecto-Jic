876a2319376af9a7a21cd11dbac645deaf49d6e7
Cris <37556966+xTryss@users.noreply.github.com>
2025-05-26 19:23:43 -0500

    Delete Desarrollo_Proyecto directory

--- Desarrollo_Proyecto/src/pages/api/mapbox-token.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  // En un entorno real, esto debería venir de variables de entorno
-  const token = process.env.MAPBOX_PUBLIC_TOKEN || 'pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2xvZ28zMWVvMDQ0eDJrcGRiZzU0bWl1biJ9.m4Vl4JzJZJZJZJZJZJZJZA';
-  
-  res.status(200).json({ token });
-}
-
-// DONE
